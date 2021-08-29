e5d39542ee5497dd1d1484abb5e514a6d845377d
김정호 <cloud0406@naver.com>
2021-08-29 22:14:00 +0900

    practice : id 태그
    * getElementById

---
+++ 인터랙티브 자바스크립트/id 태그/index.js
@@ -0,0 +1,10 @@
+// id로 태그 선택하기
+const myTag1 = document.getElementById('myNumber');
+console.log(myTag1);
+
+const myTag2 = document.getElementById('btns');
+console.log(myTag2);
+
+// 존재하지 않는 값을 태그할시 'null'값을 리턴
+const myTag3 = document.getElementById('codeit');
+console.log(myTag3);
